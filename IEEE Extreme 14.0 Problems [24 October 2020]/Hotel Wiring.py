df09415c43017c9661349c921b6aae22db6c7372
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-09-01 11:09:22 +0430

    reformated

--- IEEE Extreme 14.0 Problems [24 October 2020]/Hotel Wiring.py
+++ IEEE Extreme 14.0 Problems [24 October 2020]/Hotel Wiring.py
@@ -1,20 +1,22 @@
-t= int(input())
+# Hotel wiring question from 'v14.pdf' page 15, find the pdf file in this directory
 
+t = int(input())
 
 
 def maximaze(t):
-
     for i in range(t):
         m, n, k = map(int, input().split(" "))
-        maximum=0       # maximum number of rooms that may recieve power
-        ls1 = []        # how many rooms per floor have correct wiring
+        maximum = 0  # maximum number of rooms that may recieve power
+        ls1 = []  # how many rooms per floor have correct wiring
         for i in range(m):
             ls1.append(int(input()))
         for x in ls1:
-            if x<n:
-                maximum+=n-x
+            if x < n:
+                maximum += n - x
             else:
-                maximum+=x
+                maximum += x
         print(maximum)
     return ""
+
+
 print(maximaze(t))
